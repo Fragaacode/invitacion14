63e01dbb32ec2c4b827ef830aef16237a4e4b677
Franco López Fraga <francoagustinlopezfraga@gmail.com>
2026-02-11 17:26:54 -0300

    Mi version del code cambios imagenes y textos

---
+++ valentinesday-main/src/util/valentineConstants.js
@@ -0,0 +1,66 @@
+const GIFS = [
+    {
+        id: 1,
+        description: "No",
+        img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
+    },
+    {
+        id: 2,
+        description: "Piénsalo de nuevo.",
+        img: "https://i.pinimg.com/originals/77/6b/21/776b215bed3deeef47fd3aa657685a18.gif"
+    }
+    ,
+    {
+        id: 3,
+        description: "Daaa pone que si.",
+        img: "https://www.gifmaniacos.es/wp-content/uploads/2019/05/gatitos-kawaii-gifmaniacos.es-19.gif"
+    },
+    {
+        id: 4,
+        description: "Cagona",
+        img: "https://i.pinimg.com/originals/e1/c3/88/e1c388133e0f998e25bb17c837b74a14.gif"
+    },
+    {
+        id: 5,
+        description: "No seas kuka",
+        img: "https://media.tenor.com/Bn88VELdNI8AAAAi/peach-goma.gif"
+    },
+    {
+        id: 6,
+        description: "Voy a ir igual mejor pone que si",
+        img: "https://i.pinimg.com/originals/c6/b3/0d/c6b30d1a2dc178aeb92de63295d4ae64.gif"
+    },
+    {
+        id: 7,
+        description: "Si pones que si no te pido mas la foto del alien (mentira, pero pone que si igual) ",
+        img: "https://media.tenor.com/N2oqtqaB_G0AAAAi/peach-goma.gif"
+    },
+    {
+        id: 8,
+        description: "Porfissssssssssssssssssss",
+        img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
+    },
+    {
+        id: 9,
+        description: "Si no pones que si me voy a poner triste :(",
+        img: "https://media.tenor.com/cbEccaK9QxMAAAAi/peach-goma.gif"
+    },
+    {
+        id: 10,
+        description: "Pone que si o me voy con la otra (mentira, pone que si o me voy a quedar solo) ",
+        img: "https://i.pinimg.com/originals/db/aa/c1/dbaac13f6278b91a15e480752b8a7242.gif"
+    },
+    {
+        id: 11,
+        description: "No te compro mas huevos kinder",
+        img: "https://media.tenor.com/I7KdFaMzUq4AAAAi/peach-goma.gif"
+    }];
+
+
+const ACCEPTED_GIF = {
+    id: 1,
+    description: "¡Sí!",
+    img: "https://i.pinimg.com/originals/9b/dc/c6/9bdcc6206c1d36a37149d31108c6bb41.gif"
+}
+
+export { GIFS, ACCEPTED_GIF };
